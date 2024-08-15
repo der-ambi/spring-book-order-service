0ee3aac28c5062521f2e5ffc3266f04b77ee00c4
Christian Ambach <christian.ambach@codecentric.de>
2024-08-15 14:03:54 +0200

    8.2.3 add simple business logic

--- src/main/kotlin/de/bashburg/springbook/orderservice/order/domain/OrderService.kt
+++ src/main/kotlin/de/bashburg/springbook/orderservice/order/domain/OrderService.kt
@@ -1,4 +1,20 @@
 package de.bashburg.springbook.orderservice.order.domain
 
-class OrderService {
+import de.bashburg.springbook.orderservice.order.domain.OrderStatus.REJECTED
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@Service
+class OrderService(private val orderRepository: OrderRepository) {
+
+    fun getAllOrders(): Flux<Order> = orderRepository.findAll()
+
+    fun submitOrder(bookIsbn: String, quantity: Int): Mono<Order> =
+        Mono.just(buildRejectedOrder(bookIsbn, quantity)).flatMap(orderRepository::save)
+
+    companion object {
+        fun buildRejectedOrder(bookIsbn: String, quantity: Int): Order =
+            Order.of(bookIsbn, null, null, quantity, REJECTED)
+    }
 }
