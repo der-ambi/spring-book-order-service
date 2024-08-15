28480a18885f7ab5cce476949d0870801237700c
Christian Ambach <christian.ambach@codecentric.de>
2024-08-15 13:12:28 +0200

    initial checkin after generation

---
+++ src/test/kotlin/de/bashburg/springbook/orderservice/TestOrderServiceApplication.kt
@@ -0,0 +1,9 @@
+package de.bashburg.springbook.orderservice
+
+import org.springframework.boot.fromApplication
+import org.springframework.boot.with
+
+
+fun main(args: Array<String>) {
+    fromApplication<OrderServiceApplication>().with(TestcontainersConfiguration::class).run(*args)
+}
