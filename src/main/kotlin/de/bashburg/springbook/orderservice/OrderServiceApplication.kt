d9402b9abb295c2720f1a81a7997d6bd7e84fb40
Christian Ambach <christian.ambach@codecentric.de>
2024-08-15 14:23:02 +0200

    8.3.1 add config property to catalog URI

--- src/main/kotlin/de/bashburg/springbook/orderservice/OrderServiceApplication.kt
+++ src/main/kotlin/de/bashburg/springbook/orderservice/OrderServiceApplication.kt
@@ -1,9 +1,11 @@
 package de.bashburg.springbook.orderservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class OrderServiceApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/de/bashburg/springbook/orderservice/config/ClientProperties.kt
@@ -0,0 +1,9 @@
+package de.bashburg.springbook.orderservice.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import java.net.URI
+
+@ConfigurationProperties(prefix = "polar")
+data class ClientProperties(
+    val catalogServiceUri: URI
+)
