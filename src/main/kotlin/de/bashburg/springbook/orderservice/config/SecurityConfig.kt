88dd321a1ceaca83a8b12870c0c3a35066ba86ea
Christian Ambach <christian.ambach@codecentric.de>
2024-08-26 16:31:54 +0200

    12.3 add oauth to order service

---
+++ src/main/kotlin/de/bashburg/springbook/orderservice/config/SecurityConfig.kt
@@ -0,0 +1,31 @@
+package de.bashburg.springbook.orderservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.config.web.server.invoke
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.savedrequest.NoOpServerRequestCache
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig {
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http {
+            authorizeExchange {
+                authorize(anyExchange, authenticated)
+            }
+            oauth2ResourceServer {
+                jwt { }
+            }
+            requestCache {
+                NoOpServerRequestCache.getInstance()
+            }
+            csrf {
+                disable()
+            }
+        }
+    }
+}
--- src/test/kotlin/de/bashburg/springbook/orderservice/order/web/OrderControllerTestWebFluxTests.kt
+++ src/test/kotlin/de/bashburg/springbook/orderservice/order/web/OrderControllerTestWebFluxTests.kt
@@ -1,5 +1,6 @@
 package de.bashburg.springbook.orderservice.order.web
 
+import de.bashburg.springbook.orderservice.config.SecurityConfig
 import de.bashburg.springbook.orderservice.order.domain.Order
 import de.bashburg.springbook.orderservice.order.domain.OrderService
 import de.bashburg.springbook.orderservice.order.domain.OrderStatus
@@ -8,15 +9,23 @@ import org.mockito.BDDMockito.given
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Import
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.mockJwt
 import org.springframework.test.web.reactive.server.WebTestClient
 import reactor.core.publisher.Mono
 import kotlin.test.Test
 
 @WebFluxTest(OrderController::class)
+@Import(SecurityConfig::class)
 class OrderControllerTestWebFluxTests {
 
     @Autowired
     lateinit var webClient: WebTestClient
+
+    @MockBean
+    lateinit var reactiveJwtDecoder: ReactiveJwtDecoder
 
     @MockBean
     lateinit var orderService: OrderService
@@ -29,6 +38,9 @@ class OrderControllerTestWebFluxTests {
         given(orderService.submitOrder(orderRequest.isbn, orderRequest.quantity)).willReturn(Mono.just(expectedOrder))
 
         webClient
+            .mutateWith(
+                mockJwt().authorities(SimpleGrantedAuthority("ROLE_customer"))
+            )
             .post()
             .uri("/orders")
             .bodyValue(orderRequest)
